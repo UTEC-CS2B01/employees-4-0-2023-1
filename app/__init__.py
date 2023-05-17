71b9f32d5e74a6d4bc83db6b19b5d0fe0f4932cd
mabisrror <mabisrror@utec.edu.pe>
2023-05-17 07:25:47 -0500

    0002 - endpoint crear empleado agregado

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
-from flask import Flask
-from .models import db, setup_db
+from flask import (
+    Flask,
+    request,
+    jsonify
+)
+from .models import db, setup_db, Employee
+from .utils.utilities import allowed_file
 from flask_cors import CORS
+import os
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -14,6 +20,48 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
         response.headers.add('Access-Control-Max-Age', '15')
         return response
+    
+
+    @app.route('/employees', methods=['POST'])
+    def create_employee():
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        job_title = request.form.get('job_title')
+        department_id = request.form.get('selectDepartment')
+
+        if 'image' not in request.files:
+            return jsonify({'success': False, 'message': 'No image provided'}), 400
+        
+        file = request.files['image']
+
+        if file.filename == '':
+            return jsonify({'success': False, 'message': 'No image provided'}), 400
+        
+        if not allowed_file(file.filename):
+            return jsonify({'success': False, 'message': 'File extension not allowed'}), 400
+        
+
+        employee = Employee(first_name, last_name, job_title, department_id)
+        db.session.add(employee)
+        db.session.commit()
+        
+        cwd = os.getcwd()
+
+        employee_dir = os.path.join(app.config['UPLOAD_FOLDER'], employee.id)
+        os.makedirs(employee_dir, exist_ok=True)
+
+        upload_folder = os.path.join(cwd, employee_dir)
+
+        absolute_path = os.path.join(upload_folder, file.filename)
+        file.save(absolute_path)
+        file.close()
+
+        relative_path = os.path.join(employee_dir, file.filename)
+
+        employee.image_path = relative_path
+        db.session.commit()
+
+        return jsonify({'success': True, 'id': employee.id, 'message': 'Employee created successfully'}), 201
 
 
     return app
---
+++ app/utils/utilities.py
@@ -0,0 +1,6 @@
+ALLOW_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOW_EXTENSIONS
