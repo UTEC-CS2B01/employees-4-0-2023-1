85de3a549e4f03a3d5a059fcf004de3e53857388
marvin4keap <marvin.abisrror@keap.com>
2023-06-15 07:24:27 -0500

    UTEC-0008 remove helloWorld

---
+++ frontend/src/components/DepartmentCreation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "DepartmentCreation",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
