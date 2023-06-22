046718fc06563aa4088c16ad802695db8cc60169
RenzoA21 <85076359+RenzoA21@users.noreply.github.com>
2023-06-22 07:52:00 -0500

    Update EmployeeLists.vue

---
+++ frontend/src/components/EmployeeLists.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>List of Employees</h1>
+    <ul>
+      <li v-for="employee in employees" :key="employee.id">
+        {{ employee.firstName }} -- {{ employee.lastName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getAllEmployees } from "@/services/employees.api";
+export default {
+  name: "EmployeeList",
+  mounted() {
+    this.getEmployees();
+  },
+  data() {
+    return {
+      employees: [],
+    };
+  },
+  methods: {
+    async getEmployees() {
+      const { employees } = await getAllEmployees();
+      this.employees = employees;
+    },
+  },
+};
+</script>
+
+<style></style>
